c8af7fbc7b7f57a64312bdc5b22172be79edf1d1
Jean-Luc Jumpertz <jean-luc@celedev.eu>
2014-04-15 08:23:07 +0200

    Initial Commit

---
+++ TextKitSubclasses/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TextKitSubclasses
+//
+//  Created by Jean-Luc on 15/04/2014.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TextKitSubclasses/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TextKitSubclasses
+//
+//  Created by Jean-Luc on 15/04/2014.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
